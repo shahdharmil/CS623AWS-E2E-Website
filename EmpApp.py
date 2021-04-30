239440cd5b4c61825c838adbf9f8e816224b1303
gangareddynachu <gnachu@horizon.csueastbay.edu>
2021-04-30 02:36:44 -0700

    Update EmpApp.py

--- EmpApp.py
+++ EmpApp.py
@@ -48,7 +48,7 @@ def AddEmp():
     cursor = db_conn.cursor()
 
     if emp_image_file.filename == "":
-        return "<h1>Please select a file</h1>"
+        return render_template('Error1.html')
 
     try:
 
@@ -59,28 +59,28 @@ def AddEmp():
         emp_image_file_name_in_s3 = "emp-id-" + str(emp_id) + "_image_file"
         s3 = boto3.resource('s3')
 
+        try:
+            print("Data inserted in MySQL RDS... uploading image to S3...")
+            s3.Bucket(custombucket).put_object(Key=emp_image_file_name_in_s3, Body=emp_image_file)
+            bucket_location = boto3.client('s3').get_bucket_location(Bucket=custombucket)
+            s3_location = (bucket_location['LocationConstraint'])
 
-        print("Data inserted in MySQL RDS... uploading image to S3...")
-        s3.Bucket(custombucket).put_object(Key=emp_image_file_name_in_s3, Body=emp_image_file)
-        bucket_location = boto3.client('s3').get_bucket_location(Bucket=custombucket)
-        s3_location = (bucket_location['LocationConstraint'])
+            if s3_location is None:
+                s3_location = ''
+            else:
+                s3_location = '-' + s3_location
 
-        if s3_location is None:
-            s3_location = ''
-        else:
-            s3_location = '-' + s3_location
+            object_url = "https://s3{0}.amazonaws.com/{1}/{2}".format(
+                s3_location,
+                custombucket,
+                emp_image_file_name_in_s3)
 
-        object_url = "https://s3{0}.amazonaws.com/{1}/{2}".format(
-            s3_location,
-            custombucket,
-            emp_image_file_name_in_s3)
+        except Exception as e:
+            print(e)
+            return render_template('Error1.html')
 
-    except Exception as e:
-        print(e)
-        return render_template('Error1.html')
-
-  
-
+    finally:
+        cursor.close()
 
     print("all modification done...")
     return render_template('AddEmpOutput.html', name=emp_name)
