cd8970e38ba864d47d16ca01219a2b5e960d1a0c
Dharmil Shah <52252578+shahdharmil@users.noreply.github.com>
2022-11-26 21:22:34 -0800

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 customhost = "database-1.cerwdqhdu9jn.us-east-2.rds.amazonaws.com"
 customuser = "admin"
 custompass = "narsingh100"
-customdb = "databse-1"
+customdb = "database-1"
 custombucket = "addattendence"
 customregion = "us-east-2"
 adminusername = "admin"
