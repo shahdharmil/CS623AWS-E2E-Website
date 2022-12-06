53f1c5afdd4f786f351571a5d6b06ea150242813
Dharmil Shah <52252578+shahdharmil@users.noreply.github.com>
2022-12-06 15:43:23 -0800

    Update EmpApp.py

--- EmpApp.py
+++ EmpApp.py
@@ -53,7 +53,7 @@ def Addemp():
     pri_skill = request.form['pri_skill']
     emp_image_file = request.files['emp_image_file']
 
-    insert_sql = "INSERT INTO employee VALUES (%s, %s, %s, %s, %s)"
+    insert_sql = "INSERT INTO employee VALUES (%s, %s, %s, %s)"
     cursor = db_conn.cursor()
 
     try:
