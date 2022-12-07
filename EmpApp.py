51863ed29d19d184a717934b63ff57ff061f1c0c
Dharmil Shah <52252578+shahdharmil@users.noreply.github.com>
2022-12-06 20:21:52 -0800

    Update EmpApp.py

--- EmpApp.py
+++ EmpApp.py
@@ -140,7 +140,7 @@ def FetchEmp():
             print(result[5])
             
             return render_template("EmployeeInfo_Output.html", id=output["emp_id"], fname=output["first_name"],
-                               lname=output["last_name"], interest=output["primary_skills"], location=output["location"], image_url=image_url, subject_database = output["subject_database"])
+                               lname=output["last_name"], interest=output["primary_skills"], location=output["location"], subject_database = output["subject_database"])
             
         except Exception as e:
             print(e)
