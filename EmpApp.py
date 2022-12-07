9bb2b240880e06a5d0689abae003608434c9e224
Dharmil Shah <52252578+shahdharmil@users.noreply.github.com>
2022-12-06 21:18:06 -0800

    Update EmpApp.py

--- EmpApp.py
+++ EmpApp.py
@@ -145,49 +145,45 @@ def GetEmp():
 @app.route("/fetchdata", methods=['POST'])
 def FetchEmp():
     
-    if request.method == 'POST':
-        
-        
+    emp_id = request.form['emp_id']
+
+    output = {}
+    select_sql = "SELECT empid, fname, lname, pri_skill, location from employee where empid=%s"
+    cursor = db_conn.cursor()
+    emp_image_file_name_in_s3 = "emp-id-" + str(emp_id) + "_image_file"
+    s3 = boto3.resource('s3')
+
+    bucket_location = boto3.client('s3').get_bucket_location(Bucket=custombucket)
+    s3_location = (bucket_location['LocationConstraint'])
+
+    if s3_location is None:
+        s3_location = ''
     else:
-        emp_id = request.form['emp_id']
+        s3_location = '-' + s3_location
 
-        output = {}
-        select_sql = "SELECT empid, fname, lname, pri_skill, location from employee where empid=%s"
-        cursor = db_conn.cursor()
-        emp_image_file_name_in_s3 = "emp-id-" + str(emp_id) + "_image_file"
-        s3 = boto3.resource('s3')
+    image_url = "https://s3{0}.amazonaws.com/{1}/{2}".format(
+        s3_location,
+        custombucket,
+        emp_image_file_name_in_s3)
 
-        bucket_location = boto3.client('s3').get_bucket_location(Bucket=custombucket)
-        s3_location = (bucket_location['LocationConstraint'])
+    try:
+        cursor.execute(select_sql, (emp_id))
+        result = cursor.fetchone()
+        
 
-        if s3_location is None:
-            s3_location = ''
-        else:
-            s3_location = '-' + s3_location
+        output["emp_id"] = result[0]
+        print('EVERYTHING IS FINE TILL HERE')
+        output["first_name"] = result[1]
+        output["last_name"] = result[2]
+        output["primary_skills"] = result[3]
+        output["location"] = result[4]
+        print(output["emp_id"])
+        return render_template("EmployeeInfo_Output.html", id=output["emp_id"], fname=output["first_name"],
+                               lname=output["last_name"], interest=output["primary_skills"], location=output["location"], image_url=image_url)
 
-        image_url = "https://s3{0}.amazonaws.com/{1}/{2}".format(
-            s3_location,
-            custombucket,
-            emp_image_file_name_in_s3)
-
-        try:
-            cursor.execute(select_sql, (emp_id))
-            result = cursor.fetchone()
-            
-
-            output["emp_id"] = result[0]
-            print('EVERYTHING IS FINE TILL HERE')
-            output["first_name"] = result[1]
-            output["last_name"] = result[2]
-            output["primary_skills"] = result[3]
-            output["location"] = result[4]
-            print(output["emp_id"])
-            return render_template("EmployeeInfo_Output.html", id=output["emp_id"], fname=output["first_name"],
-                                   lname=output["last_name"], interest=output["primary_skills"], location=output["location"], image_url=image_url)
-
-        except Exception as e:
-            print(e)
-            return render_template('Error.html')
+    except Exception as e:
+        print(e)
+        return render_template('Error.html')
             
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
