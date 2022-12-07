fa00ad9c7ede0be3719e56c6d8755a73c6801da2
Dharmil Shah <52252578+shahdharmil@users.noreply.github.com>
2022-12-07 11:38:45 -0800

    Update EmpApp.py

--- EmpApp.py
+++ EmpApp.py
@@ -90,9 +90,10 @@ def AddEmp():
     last_name = request.form['last_name']
     pri_skill = request.form['pri_skill']
     location = request.form['location']
+    subject_database = request.form['subject_database']
     emp_image_file = request.files['emp_image_file']
 
-    insert_sql = "INSERT INTO employee VALUES (%s, %s, %s, %s, %s)"
+    insert_sql = "INSERT INTO employee VALUES (%s, %s, %s, %s, %s ,%s)"
     cursor = db_conn.cursor()
 
 
